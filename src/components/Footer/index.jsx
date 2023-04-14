8f4bb1a394b4a643a2b084ee8b3f004dfea53902
Santiago Volentiera <santiagovolentiera@gmail.com>
2023-04-14 13:41:54 -0300

    fix: media q and footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,7 +5,7 @@ import logo2 from '../../assets/logo2.png'
 
 export default function Footer() {
     return (
-        <div className='container-fluid text-center p-5 footer-config d-flex align-items-center'>
+        <div className='container-fluid p-5 footer-config d-flex align-items-center'>
             <div className="row">
                 <div className="d-md-none col-sm-3"></div>
                 <div className="col-sm-6 col-md-6 col-lg-3 ">
@@ -13,20 +13,29 @@ export default function Footer() {
                 </div>
                 <div className="d-md-none col-sm-3"></div>
                 <div className="col-sm-12 col-md-6 col-lg-3">
-                    <h3>Contacto:</h3>
-                    <p>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti, neque quam eius, perferendis esse, doloribus cumque vel magni eum at modi odio voluptas laudantium necessitatibus reiciendis qui in quisquam vitae.
-                    </p>
+                    <h3 className='pb-5 text-center'>Contacto:</h3>
+                    <ul>
+                        <li><p>Telefono: +54 1234567</p></li>
+                        <li><p>Pais: Argentina</p></li>
+                        <li><p>Ciudad: Rosario</p></li>
+                        <li><p>Direccion: Av Cordoba 1234</p></li>
+                    </ul>
                 </div>
                 <div className="col-sm-12 col-md-6 col-lg-3">
-                    <p>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti, neque quam eius, perferendis esse, doloribus cumque vel magni eum at modi odio voluptas laudantium necessitatibus reiciendis qui in quisquam vitae.
-                    </p>
+                    <h3 className='pb-5 text-center'>Menu:</h3>
+                    <ul>
+                        <li><a href="#contacto"><p>Contacto</p></a></li>
+                        <li><a href="#sobre-nosotros"><p>Sobre Nosotros</p></a></li>
+                        <li><a href="#servicios"><p>Servicios</p></a></li>
+                    </ul>
                 </div>
                 <div className="col-sm-12 col-md-6 col-lg-3">
-                    <p>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti, neque quam eius, perferendis esse, doloribus cumque vel magni eum at modi odio voluptas laudantium necessitatibus reiciendis qui in quisquam vitae.
-                    </p>
+                    <h3 className='pb-5 text-center'>Redes:</h3>
+                    <ul>
+                        <li className='m-3'><a className='m-3' href=""><i className="fa-brands fa-facebook"></i></a></li>
+                        <li className='m-3'><a className='m-3' href=""><i className="fa-brands fa-instagram"></i></a></li>
+                        <li className='m-3'><a className='m-3' href=""><i className="fa-brands fa-linkedin"></i></a></li>
+                    </ul>
                 </div>
             </div>
         </div>
